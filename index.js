f9d4aafb81a8c2aad4cb5381d08912c41c3c41c1
Krista Harder <kristah239@gmail.com>
2018-01-19 17:15:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 function happyHolidays {
   return ("Happy holidays!")
 }
-function happyHolidaysTo(name) {
+function happyHolidaysTo(string) {
   return ("Happy holidays, ${name}!")
 }
+function happyHolidayTo(string)
